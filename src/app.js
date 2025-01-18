e5c7a2c08fada2a01a11a525a446a6a7a7608c44
vinisf <vinisferreira95@gmail.com>
2025-01-18 11:15:23 -0300

    Atualizações

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "../views"));
+app.set('views', path.join(__dirname, 'src', 'views')); // Ajuste o caminho de acordo com a estrutura
 
 
 app.use('/upload', express.static('upload'))
